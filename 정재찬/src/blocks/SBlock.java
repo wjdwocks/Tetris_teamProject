62d83bf1b0484d64ec153009006080dd16417b18
JeongJaeChan <144427497+wjdwocks@users.noreply.github.com>
2024-03-28 20:04:35 +0900

    Add files via upload

---
+++ 정재찬/src/blocks/SBlock.java
@@ -0,0 +1,16 @@
+package blocks;
+
+import java.awt.Color;
+
+public class SBlock extends Block {
+
+	public SBlock() {
+
+		//2X3 크기의 2차원 정수 배열 설정
+		shape = new int[][] { 
+			{0, 1, 1},
+			{1, 1, 0}
+		};
+		color = Color.GREEN;
+	}
+}
